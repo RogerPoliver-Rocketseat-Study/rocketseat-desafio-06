804a4c9ed899928a51a710d08835adafb35102cd
Roger Oliveira <rogerpolvr@gmail.com>
2020-07-20 23:39:48 -0300

    Implementando a listagem e criaçao de uma transaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,39 @@
 // import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  private transactionsRepository: TransactionsRepository;
+
+  constructor() {
+    // this.categoriesRepository = getCustomRepository(CategoriesRepository);
+    this.transactionsRepository = getCustomRepository(TransactionsRepository);
+  }
+
+  public async execute({
+    title,
+    type,
+    value,
+    category,
+  }: Request): Promise<Transaction> {
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+
+    await this.transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
